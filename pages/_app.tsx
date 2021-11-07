78ce6b73667feccb0b4c414f00b85901ffde9ea1
yuki.kakihara <argentina.kyg@gmail.com>
2021-11-07 21:17:48 +0900

    Footerを消した

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { imagePath } from 'utils/imagePath';
-import { Footer } from 'uiParts/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -16,7 +15,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href={imagePath('favicon.ico')} />
       </Head>
       <Component {...pageProps} />
-      <Footer />
     </>
   );
 }
