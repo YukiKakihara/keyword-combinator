c5a69d321aea2320684f2b3f264b46163c2a62c3
YukiKakihara <YukiKakihara@users.noreply.github.com>
2021-11-07 11:10:56 +0000

    deploy: 9c55579a05c380551576ff64d2e9400baca226f7

--- _next/static/mle1s6rwoq4ilgQstuVd-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/910-81deefd8c3972bbf.js","static/chunks/pages/index-819a50e9fb9033f3.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
