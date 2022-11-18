b881a58c8fe4e59e94d623995f612e458ee607b5
YukiKakihara <YukiKakihara@users.noreply.github.com>
2022-11-18 23:52:10 +0000

    deploy: b8b3b547d8a97627d2dc472e9b01e063e1a20bfe

---
+++ _next/static/D770r6dOVqsFaL350A7ZS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/653-28237b12895b428b.js","static/chunks/pages/index-30082ec8dd1a8249.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/ofc-eJKovjxiD1I6P-aXM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/653-3ce31427fc331839.js","static/chunks/pages/index-073cd9922c909ef1.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
